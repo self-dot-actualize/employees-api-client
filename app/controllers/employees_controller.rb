9ee9db17ef26ec36fe12cde9f017dfa70b9cadd4
Peter Jang <peter.x.jang@gmail.com>
2017-04-06 18:52:40 -0500

    Add employees index, new, create actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,27 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees").body
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @employee = Unirest.post(
+      "http://localhost:3000/api/v1/employees",
+      parameters: {
+        first_name: params["form_first_name"],
+        last_name: params["form_last_name"],
+        birthdate: params["form_birthdate"],
+        email: params["form_email"],
+        ssn: params["form_ssn"]
+      }
+    ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}").body
     render "show.html.erb"
